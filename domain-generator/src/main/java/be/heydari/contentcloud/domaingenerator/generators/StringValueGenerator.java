6edee668ce7ca6bc2c6b6aa7b40960aaf7be87f7
Martijn Sauwens <martijn.sauwens@gmail.com>
2021-03-23 11:24:14 +0100

    basic helm charts

---
+++ domain-generator/src/main/java/be/heydari/contentcloud/domaingenerator/generators/StringValueGenerator.java
@@ -0,0 +1,9 @@
+package be.heydari.contentcloud.domaingenerator.generators;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public interface ValueGenerator extends GenericValueGenerator<String> {
+
+}
